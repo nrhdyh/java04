41ad6461688dc6e2f9367ace1ef82944843ab65a
user <user@LAPTOP-INIHI79H>
2018-01-17 07:20:09 +0800

    java04.1

---
+++ src/jenis.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class jenis {
+    public static void main(String[] args) {
+        int bilangan=139;
+        String barangan="Biskut";
+        double harga=2.55;
+        boolean status=true;
+        float suhu=4.23468923f;
+        char pilihan='Y';
+        
+        System.out.println("Stok semasa:"+bilangan +" unit");
+        System.out.println("Nama barangan:"+barangan );
+        System.out.println("Harga seunit :RM:"+harga);
+        System.out.println("Status barangan:"+status);
+        System.out.println("Suhu baranagan:"+suhu);
+        System.out.println("Semua maklumat adalah benar:"+pilihan); 
+       
+        
+    }
+}
+
